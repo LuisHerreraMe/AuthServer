aaee1e95705a8dfd16278f060b78fdf15afec79d
Luis David Herrera Medina <luisherreramedina950@gmail.com>
2024-10-31 18:26:16 -0500

    IMPLEMENTACION DE PERMISOS

--- src/main/kotlin/com/kuby/plugins/Cors.kt
+++ src/main/kotlin/com/kuby/plugins/Cors.kt
@@ -9,7 +9,11 @@ fun Application.configureCors() {
     install(CORS) {
         anyHost() // Solo en desarrollo, en producción usa allowHost("https://frontend.com")
         allowMethod(HttpMethod.Options) // Permitir preflight requests
-        allowMethod(HttpMethod.Post) // Método POST permitido
+        allowMethod(HttpMethod.Get) // Permitir metodo GET
+        allowMethod(HttpMethod.Post) // Permitir metodo POST
+        allowMethod(HttpMethod.Put) // Permitir metodo PUT
+        allowMethod(HttpMethod.Delete) // Permitir metodo DELETE
+        allowMethod(HttpMethod.Patch) // Permitir metodo PATCH
         allowHeader(HttpHeaders.ContentType) // Permitir header para JSON
         allowHeader(HttpHeaders.Authorization) // Si usas autenticación por token
         allowCredentials = true // Si envías cookies o credenciales
